49f5c60e52981135893c27830619c0def2a2573e
unknown <289823411@qq.com>
2019-06-28 11:19:23 +0800

    add jj

---
+++ src/views/citySupplier.vue
@@ -0,0 +1,31 @@
+<template>
+    <el-table
+            :data="tableData"
+            style="width: 50%">
+        <el-table-column
+                prop="name"
+                label="城市"
+                width="180">
+        </el-table-column>
+        <el-table-column
+                prop="address"
+                label="商户数">
+        </el-table-column>
+    </el-table>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                tableData: [{
+                    name: '南昌',
+                    address: 100
+                }, {
+                    name: '山西',
+                    address: 5
+                }]
+            }
+        }
+    }
+</script>
