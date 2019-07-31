104c156f29a7094b5baeb154e666985de594052b
tianliangleng <2438001922@qq.com>
2019-07-31 17:59:23 +0800

    ww

--- src/views/userMenage/operator.vue
+++ src/views/userMenage/operator.vue
@@ -15,12 +15,25 @@
           end-placeholder="结束日期"
           :picker-options="pickerOptions"
         ></el-date-picker>
-        <el-button type="primary" icon="el-icon-search" size="small" @click="search">搜索</el-button>
+        <el-radio-group v-model="day" size="mini" @change="getVestJobs">
+          <el-radio-button label="today">今天</el-radio-button>
+          <el-radio-button label="yesterday">昨天</el-radio-button>
+          <el-radio-button label="threedays">近三天</el-radio-button>
+          <el-radio-button label="week">近一周</el-radio-button>
+        </el-radio-group>
+        <el-button type="success" icon="el-icon-search" size="mini" @click="search" style="margin:0 0 0 10px;">搜索</el-button>
         <el-button type="success" size="mini" @click="beforeExport" icon="iconfont daochu">导出</el-button>
       </div>
     </div>
     <div class="content">
-      <el-table :data="dataList.list" stripe border style="width:100%" v-loading="loading" class="need-table">
+      <el-table
+        :data="dataList.list"
+        stripe
+        border
+        style="width:100%"
+        v-loading="loading"
+        class="need-table"
+      >
         <el-table-column prop="id" label="用户id" align="center"></el-table-column>
         <el-table-column prop label="头像" align="center">
           <template slot-scope="scope">
@@ -33,10 +46,9 @@
           >{{scope.row.username}}{{scope.row.nickname ?"("+scope.row.nickname+")": ''}}</template>
         </el-table-column>
         <el-table-column prop="need" label="发布需求数" align="center"></el-table-column>
-        <el-table-column prop="unum" label="马甲消费者数量" align="center"></el-table-column> 
+        <el-table-column prop="unum" label="马甲消费者数量" align="center"></el-table-column>
         <el-table-column prop="reply" label="回复需求数" align="center"></el-table-column>
         <el-table-column prop="gnum" label="马甲专引师数量" align="center"></el-table-column>
-         
       </el-table>
       <el-pagination
         background
@@ -94,13 +106,13 @@ export default {
       page: 1,
       limit: 10,
       times: "",
-      shopname: "",
+      day:'today',
+      shopname: ""
     };
   },
   watch: {},
   components: {},
   methods: {
-
     //导出之前
     beforeExport() {
       this.$confirm("确定导出当前页数据吗？(选择100条/页试试)")
@@ -111,10 +123,10 @@ export default {
     },
     //导出
     exportExcel() {
-      console.log("下载")
+      console.log("下载");
       /* generate workbook object from table */
       var wb = XLSX.utils.table_to_book(document.querySelector(".need-table"));
-console.log(wb);
+      console.log(wb);
       /* get binary string as output */
       var wbout = XLSX.write(wb, {
         bookType: "xlsx",
@@ -132,7 +144,6 @@ console.log(wb);
       return wbout;
     },
 
-
     //获取数据列表
     getDataList() {
       this.loading = true;
@@ -140,7 +151,8 @@ console.log(wb);
         .getVestInfo({
           page: this.page,
           limit: this.limit,
-          times: this.times,
+          date: this.times,
+          day:this.day
         })
         .then(res => {
           this.dataList = res.data || [];
@@ -163,6 +175,11 @@ console.log(wb);
     // 查询
     search() {
       this.page = 1;
+      this.getDataList();
+    },
+    getVestJobs(){
+      this.page = 1;
+      this.times='';
       this.getDataList();
     }
   },
